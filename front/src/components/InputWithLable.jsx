9fb7814b334000bd334baab864e63fe9d843bedd
ghada <gh.abassi01@gmail.com>
2024-05-12 13:02:51 +0100

    FetchingDataFromApi+AddingReusableComponent

---
+++ front/src/components/InputWithLable.jsx
@@ -0,0 +1,20 @@
+//Composant Reutilisable
+
+const InputWithLabel = ({ 
+    id, 
+    value,
+    type = 'text', 
+    onInputChange,
+   children,
+    }) => (
+    <>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
+    <input
+    id={id}
+    type={type} 
+    value={value}
+    onChange={onInputChange}
+    />
+    </>
+   );
