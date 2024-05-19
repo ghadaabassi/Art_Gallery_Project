3f2b4eebb2dc3775eb23a22ec7f3fb626415f885
ghada <gh.abassi01@gmail.com>
2024-05-20 00:01:11 +0100

    fixes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,6 +7,92 @@ import InputWithLabel from "./components/InputWithLable";
 function Artwork({ artwork }) {
   return (
     <div>
+      <h2>{artwork.title}</h2>
+      <p>Artist: {artwork.artist_display}</p>
+      <p>Date: {artwork.date_display}</p>
+   
+      {artwork.image_id && (
+        <img
+          src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+          alt={artwork.title}
+        />
+      )}
+    </div>
+  );
+}
+
+function App() {
+  const [artworks, setArtworks] = useState([]);
+  const [searchTerm, setsearchTerm] = useState(
+    localStorage.getItem("search") || "Art"
+  );
+
+  const API_ENDPOINT = "https://api.artic.edu/api/v1/artworks";
+
+  //update the search term
+  const handleSearch = (event) => {
+    setsearchTerm(event.target.value);
+  };
+
+  // update the fetched data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_ENDPOINT}`);
+        const { data } = await response.json();
+
+        setArtworks(data);
+
+        // Set the state with the fetched artwork details
+      } catch (error) {
+        console.error("Error fetching search data:", error);
+      }
+    };
+    fetchData();
+  }, [searchTerm]);
+
+  //update the search value in the localStorage
+  useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
+
+  return (
+    <>
+      <h1>Artworks from the Art Institute of Chicago</h1>
+      <div>
+        {/*The Reusable Component*/}
+        <InputWithLabel
+          id="search"
+          value={searchTerm}
+          onInputChange={handleSearch}
+        >
+          <strong>Search an artwork :</strong>
+        </InputWithLabel>
+
+        {
+          //Mapping the fetched data
+          artworks.map((artwork) => (
+            //console.log(artwork);
+            <Artwork key={artwork.id} artwork={artwork} />
+          ))
+        }
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+/*
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import InputWithLabel from "./components/InputWithLable";
+
+// Dislaying the data from the api
+
+function Artwork({ artwork }) {
+  return (
+    <div>
       <h2>{artwork.data.title}</h2>
       <p>Artist: {artwork.data.artist_display}</p>
       <p>Date: {artwork.date_display}</p>
@@ -73,7 +159,8 @@ function App() {
     <>
       <h1>Artworks from the Art Institute of Chicago</h1>
       <div>
-        {/*The Reusable Component*/}
+        {//The Reusable Component
+        }
         <InputWithLabel
           id="search"
           value={searchTerm}
@@ -95,3 +182,4 @@ function App() {
 }
 
 export default App;
+*/
